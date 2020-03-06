737645d37d4bf0ab77db582cb3a825c216756589
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-06 19:03:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,12 @@ def add_element_to_end_of_array(array, element)
   
 end
 
-def remove_element_from_end_of_array(array)
+def add_element_to_start_of_array (array, elements)
   array.unshift(element)
+end 
+
+def remove_element_from_end_of_array(array)
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
