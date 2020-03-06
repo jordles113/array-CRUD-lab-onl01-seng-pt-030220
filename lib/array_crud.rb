c791a1f201a9d72435f705d8eaa41bc60d2ea480
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-06 18:59:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
   
 end
 
-def add_element_to_end_of_array(element, Array)
-  add_element_to_end_of_array.push
+def add_element_to_end_of_array(array, element)
+  
+ array << element
   
 end
 
