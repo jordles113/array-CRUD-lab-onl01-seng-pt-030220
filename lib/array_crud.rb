993da16836709f2cf5c70fa6c870f9e6f88534d3
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-06 19:04:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
   
 end
 
-def add_element_to_start_of_array (array, elements)
+def add_element_to_start_of_array(array, elements)
   array.unshift(element)
 end 
 
@@ -27,13 +27,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.index(1)
 end
 
 def retrieve_first_element_from_array(array)
+  array.first
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
